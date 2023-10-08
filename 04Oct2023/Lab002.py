9a530fa3c866d0cd173c68bf852b05d043041634
raghu1006 <raghu.c1987@gmail.com>
2023-10-08 16:38:44 +0530

    Initial commit

---
+++ 04Oct2023/Lab002.py
@@ -0,0 +1,12 @@
+#Task 1:
+name1 = input("enter your name:")
+print(name1)
+###############################
+#Task 2:
+firstname = input("enter firstname:")
+lastname = input("enter lastname:")
+print(firstname,lastname,sep=" - ",end="\t""\n")
+###############################
+#Task 3:
+name2 = input("Enter your name:",)
+print("You are welcome",name2,"to the Python Class.")
---
+++ 2Oct2023/Lab001.py
@@ -0,0 +1,3 @@
+print("Hello World!!")
+print(23+45)
+print(23*45)
